337082702a00fde62f852a8d5b6c5a2a92414516
Mark Daniel Edillor <edillormark2@gmail.com>
2024-05-03 14:08:30 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./App.css";
+
+const App = () => {
+  return <div>welcome to CinqueDev Solutions</div>;
+};
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,57 @@
+module.exports = {
+  mode: "jit",
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      display: ["DS-Digital", "sans-serif"],
+      body: ["Open Sans", "sans-serif"]
+    },
+    extend: {
+      fontSize: {
+        14: "14px"
+      },
+      backgroundColor: {
+        "main-bg": "#FAFBFB",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "on-secondary-dark-bg": "#394648",
+        "light-gray": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)"
+      },
+      borderWidth: {
+        1: "1px"
+      },
+      borderColor: {
+        color: "rgba(0, 0, 0, 0.1)"
+      },
+      width: {
+        400: "400px",
+        760: "760px",
+        780: "780px",
+        800: "800px",
+        1000: "1000px",
+        1200: "1200px",
+        1400: "1400px"
+      },
+      height: {
+        80: "320px"
+      },
+      minHeight: {
+        590: "590px"
+      },
+      backgroundImage: {
+        "hero-pattern": "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')"
+      },
+      colors: {
+        primary: "#1A97F5",
+        secondary: {
+          100: "#E2E2D5",
+          200: "#888883"
+          // Add more shades if needed
+        }
+      }
+    }
+  },
+  plugins: []
+};
