2b335d37638a5cc84fc9fe1ac7fc1ad686df058d
Mark Daniel Edillor <edillormark2@gmail.com>
2024-05-09 07:59:30 +0800

    Adding services page second part

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from "react";
 import customsoftware from "../assets/custom-software.png";
 import readysoftware from "../assets/ready-software.png";
+import update from "../assets/update.png";
+import security from "../assets/security-icon.png";
+import performance from "../assets/performance.png";
+import adaptability from "../assets/adaptability.png";
 import { FaCheck } from "react-icons/fa";
 
 const Services = () => {
@@ -99,13 +103,78 @@ const Services = () => {
               </div>
             </div>
           </div>
-          <div className="flex m-4 gap-4">
+          <div className="flex m-4 gap-4 ">
             <div className="p-2 w-full rounded-full text-white flex justify-center  cursor-pointer bg-gradient-to-r from-cyan-300 to-blue-500 hover:opacity-85 ">
               Select Plan
             </div>
             <div className="p-2 w-full rounded-full text-black border border-gray-300 flex justify-center  cursor-pointer bg-gray-200 hover:bg-gray-300">
               View Our Softwares
             </div>
+          </div>
+        </div>
+      </div>
+      <div className="mt-20 mb-40 mx-40">
+        <div className="flex justify-center text-2xl font-semibold my-10">
+          Add-Ons
+        </div>
+        <div className="flex w-full">
+          <div className="flex text-5xl gap-4 font-bold w-1/2 text-gray-800">
+            Ongoing support and updates?
+          </div>
+
+          <div className="w-5/12">
+            <div className="bg-blue-500 bg-opacity-20 text-opacity-80 p-4 rounded-lg drop-shadow-xl font-semibold text-blue-800">
+              When we develop solutions, you have the option for ongoing support
+              and updates to your developed solution. We provide regular updates
+              on project progress to make adjustments.
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col md:flex-row w-full mt-10">
+          <div className="w-5/12 relative">
+            <div className="absolute w-0.5 bg-gray-300 h-96 top-16 left-16" />
+            <div className="flex flex-row items-center  w-full p-2  gap-8 mx-4 my-10">
+              <div className="flex p-2 bg-primary rounded-full w-44 h-20 items-center justify-center drop-shadow-lg">
+                <img src={security} className="object-cover w-8 h-8" />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-xl font-semibold ">Security</p>
+                <p className="text-base text-gray-500 mt-2">
+                  Continuous updates protect against cyber threats. by fixing
+                  vulnerabilities and patching loopholes, ensuring data safety
+                  and system integrity.
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row items-center  w-full p-2 gap-8 mx-4 my-10">
+              <div className="flex p-2 bg-primary rounded-full w-44 h-20 items-center justify-center drop-shadow-lg">
+                <img src={performance} className="object-cover w-8 h-8" />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-xl font-semibold ">Performance</p>
+                <p className="text-base text-gray-500 mt-2">
+                  Regular updates squash bugs, enhance performance, and minimize
+                  crashes, resulting in smoother operation and increased
+                  reliability..
+                </p>
+              </div>
+            </div>
+            <div className="flex flex-row items-center  w-full p-2 gap-8 mx-4 my-10">
+              <div className="flex p-2 bg-primary rounded-full w-44 h-20 items-center justify-center drop-shadow-lg">
+                <img src={adaptability} className="object-cover w-10 h-10" />
+              </div>
+              <div className="flex flex-col">
+                <p className="text-xl font-semibold ">Adaptability</p>
+                <p className="text-base text-gray-500 mt-2">
+                  Updates introduce new features, empowering users to innovate
+                  and stay competitive by adapting to evolving needs and
+                  technologies.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-1/2 mx-auto flex justify-center">
+            <img src={update} className="object-cover w-full h-full" />
           </div>
         </div>
       </div>
