ab9cc233f08b8f0949fa7a015b52fa8d8a228493
Mark Daniel Edillor <edillormark2@gmail.com>
2024-05-10 04:04:25 +0800

    Modified responsive behaviour for services page

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -9,25 +9,30 @@ import { FaCheck } from "react-icons/fa";
 
 const Services = () => {
   return (
-    <div className="flex flex-col mt-10 justify-center h-auto w-full items-center">
+    <div className="flex flex-col mt-10 justify-center w-full items-center">
       {/*First Part */}
-      <div className="mt-8 text-3xl font-semibold">Our Services & Product</div>
-      <div className="flex flex-col lg:flex-row gap-16 md:gap-8 lg:gap-16 w-10/12 md:w-1/2 lg:w-11/12 xl:w-8/12 justify-center mt-10">
+      <div className="mt-8 text-2xl md:text-3xl font-semibold">
+        Our Services & Product
+      </div>
+      <div className="flex flex-col lg:flex-row gap-16 md:gap-8 lg:gap-16 w-11/12 md:w-1/2 lg:w-11/12 xl:w-8/12 justify-center mt-10">
         <div className="relative flex flex-col p-4 bg-white border hover:border-primary rounded-lg drop-shadow-xl w-full lg:w-2/5 overflow-hidden transform transition-transform hover:scale-105">
-          <p className="flex justify-center text-2xl font-bold">
+          <p className="flex justify-center text-xl md:text-2xl font-bold">
             Custom Software
           </p>
 
           <div className="flex justify-center">
-            <img src={customsoftware} className="object-cover w-80 h-80" />
+            <img
+              src={customsoftware}
+              className="object-cover w-72 md:w-80 h-72 md:h-80"
+            />
           </div>
-          <div className="mt-4 mx-10">
+          <div className="mt-4 mx-4 md:mx-10">
             <div className="mt-4">
               <div className="flex items-center gap-4 my-3 ">
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Tailored Solutions
                 </p>
               </div>
@@ -35,7 +40,7 @@ const Services = () => {
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Competitive Advantage
                 </p>
               </div>
@@ -43,7 +48,7 @@ const Services = () => {
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Scalable and Flexible
                 </p>
               </div>
@@ -51,7 +56,7 @@ const Services = () => {
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Full Control
                 </p>
               </div>
@@ -62,19 +67,22 @@ const Services = () => {
           </div>
         </div>
         <div className="flex flex-col p-4 bg-white border hover:border-primary rounded-lg drop-shadow-xl w-full lg:w-2/5 overflow-hidden transform transition-transform hover:scale-105">
-          <p className="flex justify-center text-2xl font-bold">
+          <p className="flex justify-center text-xl md:text-2xl font-bold">
             Ready Software
           </p>
           <div className="flex justify-center">
-            <img src={readysoftware} className="object-cover w-80 h-80" />
+            <img
+              src={readysoftware}
+              className="object-cover w-72 md:w-80 h-72 md:h-80"
+            />
           </div>
-          <div className="mt-4 mx-10">
+          <div className="mt-4 mx-4 md:mx-10">
             <div className="mt-4">
               <div className="flex items-center gap-4 my-3 ">
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Cost-Effectiveness
                 </p>
               </div>
@@ -82,7 +90,7 @@ const Services = () => {
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Industry Best Practices
                 </p>
               </div>
@@ -90,7 +98,7 @@ const Services = () => {
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Scalability
                 </p>
               </div>
@@ -98,13 +106,13 @@ const Services = () => {
                 <div className="p-2 bg-green-100 rounded-full border border-green-300">
                   <FaCheck className="text-green-500" />
                 </div>
-                <p className="text-lg font-semibold text-gray-500">
+                <p className="text-base md:text-lg font-semibold text-gray-500">
                   Quick Deployment
                 </p>
               </div>
             </div>
           </div>
-          <div className="flex m-4 gap-4 ">
+          <div className="flex flex-col md:flex-row m-4 gap-4 ">
             <div className="p-2 w-full rounded-full text-white flex justify-center  cursor-pointer bg-gradient-to-r from-cyan-300 to-blue-500 hover:opacity-85 ">
               Select Plan
             </div>
@@ -116,17 +124,17 @@ const Services = () => {
       </div>
 
       {/*Second Part */}
-      <div className="mt-20 mb-40 mx-8 md:mx-10 lg:mx-16 xl:mx-40">
+      <div className="mt-20 mb-40 mx-4 md:mx-10 lg:mx-16 xl:mx-40">
         <div className="flex justify-center text-2xl font-semibold my-10">
           Add-Ons
         </div>
-        <div className="flex flex-col gap-8 lg:flex-row w-full">
-          <div className="flex text-5xl font-bold w-full lg:w-1/2 text-gray-800 ">
+        <div className="flex flex-col  gap-8 lg:flex-row w-full">
+          <div className="text-center md:text-start  text-3xl md:text-5xl font-bold w-full lg:w-1/2 text-gray-800 ">
             Ongoing support and updates?
           </div>
 
           <div className="w-full lg:w-5/12">
-            <div className="bg-blue-500 bg-opacity-20 text-opacity-80 p-4 rounded-lg drop-shadow-xl font-semibold text-blue-800">
+            <div className="bg-blue-500 text-center md:text-justify bg-opacity-20 text-opacity-80 p-4 rounded-lg drop-shadow-xl font-semibold text-blue-800">
               When we develop solutions, you have the option for ongoing support
               and updates to your developed solution. We provide regular updates
               on project progress to make adjustments.
@@ -134,41 +142,43 @@ const Services = () => {
           </div>
         </div>
         <div className="flex flex-col lg:flex-row w-full mt-10">
-          <div className="w-full lg:w-5/12 relative">
-            <div className="absolute w-0.5 bg-gray-300 h-96 md:h-80 lg:h-96 xl:h-96 top-20 lg:top-32 xl:top-20 left-12 md:left-16" />
-            <div className="flex flex-row items-center w-full p-2  gap-8 mx-0 md:mx-4 my-10">
+          <div className="w-full lg:w-5/12 ">
+            <div className="relative flex flex-row items-center w-full p-0 md:p-2  gap-8 mx-0 md:mx-4 my-6 sm:my-10 md:my-8">
+              <div className="absolute w-0.5 bg-gray-300 h-60 top-10 left-10 md:left-12" />
               <div className="flex p-2 bg-primary rounded-full min-w-20 max-w-20 h-20 items-center justify-center drop-shadow-lg">
                 <img src={security} className="object-cover w-8 h-8" />
               </div>
               <div className="flex flex-col">
                 <p className="text-xl font-semibold ">Security</p>
-                <p className="text-base text-gray-500 mt-2">
+                <p className="text-sm sm:text-base text-gray-500 mt-2">
                   Continuous updates protect against cyber threats. by fixing
                   vulnerabilities and patching loopholes, ensuring data safety
                   and system integrity.
                 </p>
               </div>
             </div>
-            <div className="flex flex-row items-center  w-full p-2 gap-8 mx-0 md:mx-4 my-10">
+            <div className="relative flex flex-row items-center  w-full p-0 md:p-2 gap-8 mx-0 md:mx-4 my-6 sm:my-10 md:my-8 z-10">
+            <div className="absolute w-0.5 bg-gray-300 h-40 lg:h-52 top-10 left-10 md:left-12" />
               <div className="flex p-2 bg-primary rounded-full min-w-20 max-w-20 h-20 items-center justify-center drop-shadow-lg">
                 <img src={performance} className="object-cover w-8 h-8" />
               </div>
               <div className="flex flex-col">
                 <p className="text-xl font-semibold ">Performance</p>
-                <p className="text-base text-gray-500 mt-2">
+                <p className="text-sm sm:text-base text-gray-500 mt-2">
                   Regular updates squash bugs, enhance performance, and minimize
                   crashes, resulting in smoother operation and increased
-                  reliability..
+                  reliability.
                 </p>
               </div>
             </div>
-            <div className="flex flex-row items-center  w-full p-2 gap-8 mx-0 md:mx-4 my-10">
+            <div className="relative flex flex-row items-center  w-full p-0 md:p-2 gap-8 mx-0 md:mx-4 my-10 sm:my-10 md:my-8 z-10">
+            <div className="absolute w-0.5 bg-gray-300 h-10 md:h-20 bottom-14 left-10 md:left-12" />
               <div className="flex p-2 bg-primary rounded-full min-w-20 max-w-20 h-20 items-center justify-center drop-shadow-lg">
                 <img src={adaptability} className="object-cover w-10 h-10" />
               </div>
               <div className="flex flex-col">
                 <p className="text-xl font-semibold ">Adaptability</p>
-                <p className="text-base text-gray-500 mt-2">
+                <p className="text-sm sm:text-base text-gray-500 mt-2">
                   Updates introduce new features, empowering users to innovate
                   and stay competitive by adapting to evolving needs and
                   technologies.
