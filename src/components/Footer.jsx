b3fc857dccc07f20cddb81fb1dec5ed77f14869f
Mark Daniel Edillor <edillormark2@gmail.com>
2024-05-14 12:33:55 +0800

    Adding footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,37 +4,45 @@ import { MdEmail } from "react-icons/md";
 import { IoLogoLinkedin } from "react-icons/io";
 import simplifiedlogo from "../assets/cds-simplified-logo.png";
 import { Divider } from "@mui/joy";
+import { Link, useNavigate } from "react-router-dom";
 
 function Footer() {
+  const navigate = useNavigate();
+
+  const handleClick = path => {
+    navigate(path);
+    window.scrollTo({ top: 0 });
+  };
+
   return (
-    <div className="bg-white  w-full flex md:flex-row flex-col justify-between items-start py-8  lg:py-16 px-8 lg:px-40 border border-gray-300">
+    <div className="bg-white w-full flex md:flex-row flex-col justify-between items-start py-8 lg:py-16 px-8 lg:px-40 border border-gray-300">
       <div className="w-full md:w-1/2 md:justify-center">
         <ul>
-          <div className="flex items-center gap-4 mb-4 ">
+          <div className="flex items-center gap-4 mb-4">
             <div className="p-2 bg-gray-200 rounded-xl min-w-20 max-w-24 my-4 cursor-pointer shadow-xl hover:shadow-primary">
               <img
                 src={simplifiedlogo}
                 alt="logo"
-                className="w-auto h-16 object-cover "
+                className="w-auto h-16 object-cover"
               />
             </div>
-            <p className="text-gray-500 pb-6 w-full sm:w-4/6 dark:text-gray-300 md:text-md text-sm mt-4 ">
+            <p className="text-gray-500 pb-6 w-full sm:w-4/6 dark:text-gray-300 md:text-md text-sm mt-4">
               © 2024 CinqueDev Solutions. All rights reserved. For inquiries or
               questions, feel free to contact us.
             </p>
           </div>
           <Divider />
           <div className="flex gap-6 pb-5 mt-4">
-            <div className=" p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary  hover:text-white cursor-pointer">
+            <div className="p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary hover:text-white cursor-pointer">
               <FaFacebook className="text-2xl" />
             </div>
-            <div className="  p-2 drop-shadow-lg rounded-full bg-gray-200  hover:bg-primary  hover:text-white cursor-pointer">
+            <div className="p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary hover:text-white cursor-pointer">
               <FaGithub className="text-2xl" />
             </div>
-            <div className=" p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary  hover:text-white cursor-pointer">
+            <div className="p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary hover:text-white cursor-pointer">
               <MdEmail className="text-2xl" />
             </div>
-            <div className="  p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary  hover:text-white cursor-pointer">
+            <div className="p-2 drop-shadow-lg rounded-full bg-gray-200 hover:bg-primary hover:text-white cursor-pointer">
               <IoLogoLinkedin className="text-2xl" />
             </div>
           </div>
@@ -44,35 +52,47 @@ function Footer() {
       <div className="flex justify-end gap-8 lg:gap-10">
         <div>
           <ul className="p-4">
-            <p className="text-gray-800  font-bold text-md  md:text-xl pb-4">
+            <p className="text-gray-800 font-bold text-md md:text-xl pb-4">
               Quick Links
             </p>
-            <li className="text-gray-500 pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer text-sm">
+            <li
+              className="text-gray-500 pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer text-sm"
+              onClick={() => handleClick("/")}
+            >
               Home
             </li>
-            <li className="text-gray-500   text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
+            <li
+              className="text-gray-500 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer"
+              onClick={() => handleClick("/services")}
+            >
               Services
             </li>
-            <li className="text-gray-500   text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
+            <li
+              className="text-gray-500 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer"
+              onClick={() => handleClick("/about")}
+            >
               About
             </li>
-            <li className="text-gray-500   text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
+            <li
+              className="text-gray-500 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer"
+              onClick={() => handleClick("/contact")}
+            >
               Contact
             </li>
           </ul>
         </div>
         <div>
           <ul className="p-4">
-            <p className="text-gray-800 dark:text-gray-200 font-bold text-md  md:text-xl pb-4">
+            <p className="text-gray-800 dark:text-gray-200 font-bold text-md md:text-xl pb-4">
               Information
             </p>
             <li className="text-gray-500 dark:text-gray-300 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
               Features
             </li>
-            <li className="text-gray-500 dark:text-gray-300  text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
+            <li className="text-gray-500 dark:text-gray-300 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
               FAQ
             </li>
-            <li className="text-gray-500  dark:text-gray-300 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
+            <li className="text-gray-500 dark:text-gray-300 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
               Privacy policy
             </li>
             <li className="text-gray-500 dark:text-gray-300 text-sm pb-2 font-semibold hover:text-primary dark:hover:text-primary cursor-pointer">
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useRef } from "react";
 import homepage from "../assets/homepage.png";
 import codingillu from "../assets/codingillu.png";
 import devteamillu from "../assets/devteamillu.png";
-import logo from "../assets/cds-simplified-logo.png";
 import Footer from "../components/Footer";
 
 const Home = () => {
+  const secondPartRef = useRef(null);
+
+  const handleReadMoreClick = () => {
+    if (secondPartRef.current) {
+      secondPartRef.current.scrollIntoView({ behavior: "smooth" });
+    }
+  };
+
   return (
     <div>
-      {/*Home page first part */}
+      {/* Home page first part */}
       <div className="my-0 lg:my-20 mx-4 lg:mx-10 xl:mx-40">
         <div className="flex flex-col lg:flex-row w-full gap-10 xl:gap-20 mt-20 lg:mt-0">
           <div className="w-full lg:w-1/2 mt-4 md:mt-32">
@@ -26,7 +33,10 @@ const Home = () => {
               >
                 Schedule a Free Consultation
               </div>
-              <div className="mx-auto md:mx-0 max-w-32 py-2 px-4 bg-white rounded-full drop-shadow-lg cursor-pointer hover:bg-blue-50 border hover:border-gray-300 font-semibold text-gray-500">
+              <div
+                className="mx-auto md:mx-0 max-w-32 py-2 px-4 bg-white rounded-full drop-shadow-lg cursor-pointer hover:bg-blue-50 font-semibold text-gray-500"
+                onClick={handleReadMoreClick}
+              >
                 Read more
               </div>
             </div>
@@ -39,24 +49,24 @@ const Home = () => {
           </div>
         </div>
       </div>
-      {/*Home page second part */}
-      <div>
+      {/* Home page second part */}
+      <div ref={secondPartRef}>
         <div className="my-0 lg:my-20 mx-2 lg:mx-20">
-          <div className="p-4 lg:p-8 xl:p-14 2xl:p-20 flex  flex-col-reverse lg:flex-row gap-10">
+          <div className="p-4 lg:p-8 xl:p-14 2xl:p-20 flex flex-col-reverse lg:flex-row gap-10">
             <div className="w-full lg:w-1/2 flex justify-center">
               <div className="flex flex-col gap-16">
                 <img
                   src={codingillu}
                   className="w-72 lg:w-72 h-60 lg:h-72 object-contain"
                 />
-                <div className="w-32 bg-blue-500 text-blue-800 bg-opacity-20 text-opacity-80 rounded-xl shadow-lg p-3 text-center mx-auto ">
-                  <p className="text-4xl font-bold ">90%</p>
+                <div className="w-32 bg-blue-500 text-blue-800 bg-opacity-20 text-opacity-80 rounded-xl shadow-lg p-3 text-center mx-auto">
+                  <p className="text-4xl font-bold">90%</p>
                   <p className="font-semibold">Success Score</p>
                 </div>
               </div>
               <div className="flex flex-col gap-16">
                 <div className="w-32 bg-blue-500 text-blue-800 bg-opacity-20 text-opacity-80 rounded-xl shadow-lg p-2 text-center mx-auto">
-                  <p className="text-4xl font-bold ">3+</p>
+                  <p className="text-4xl font-bold">3+</p>
                   <p className="font-semibold">Years of Experience</p>
                 </div>
                 <img
@@ -70,7 +80,7 @@ const Home = () => {
                 Why choose CinqueDev Solutions For Your Innovative Services?
               </p>
               <div className="h-0.5 w-24 flex mx-auto lg:mx-0 bg-gradient-to-r from-cyan-300 to-blue-500 my-6" />
-              <p className="mr-0 xl:mr-20 text-center lg:text-justify my-4 text-lg  text-gray-500">
+              <p className="mr-0 xl:mr-20 text-center lg:text-justify my-4 text-lg text-gray-500">
                 We, as a custom software and ready software development company
                 process highly robust technological solutions for your business
                 needs. The numbers here prove that we are a leading partner when
